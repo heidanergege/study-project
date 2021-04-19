b8dbeb027a777afcc53a86b8a38c1ce24cacd8bc
changjian <88297155@qq.com>
2021-04-19 21:39:22 +0800

    课堂练习与作业

---
+++ 四月份JavaScript/210419-网页轮播图的实现/作业轮播图/js/animate.js
@@ -0,0 +1,55 @@
+        // 缓动动画函数封装 obj(目标对象), target(目标位置)
+        // 实现思路：
+        // 1.让盒子每次移动的距离慢慢变小
+        // 2. 核心算法(公式)：(目标值 - 现在的位置) / 10  得出每次移动的步长
+        // 3.停止的条件是：让当前盒子的位置 等于 目标位置 解停止定时器
+        function animate(obj, target, callback) {
+            //bug ：当我们不断的点击按钮，相当于开启了太多的定时器，这个元素的速度会越来越快
+            // 解决方法：先清除以前的定时器，让元素只有一个定时器执行 如下：
+            clearInterval(obj.timer);
+            obj.timer = setInterval(function () {
+                // 步长公式因为有 现在的位置 的参数 所以要写到定时器里面
+                // 步长值改为整数，不要出现小数，否则就不准了 Math.ceil()
+                // 先声明一个变量
+                var step = (target - obj.offsetLeft) / 10;
+                // 考虑盒子需要向左移动 也就是 步长为负值，此时就需要往小取整 Math.floor()
+                step = step > 0 ? Math.ceil(step) : Math.floor(step);
+                if (obj.offsetLeft == target) {
+                    //停止动画 本质是停止定时器
+                    clearInterval(obj.timer);
+                    // 如果callback 为 true 则调用名为 callback 的这个函数
+                    if (callback) {
+                        callback();
+                    }
+                }
+                obj.style.left = obj.offsetLeft + step + 'px';
+            }, 15)
+        }
+        
+                // 缓动动画函数封装 obj(目标对象), target(目标位置)
+        // 实现思路：
+        // 1.让盒子每次移动的距离慢慢变小
+        // 2. 核心算法(公式)：(目标值 - 现在的位置) / 10  得出每次移动的步长
+        // 3.停止的条件是：让当前盒子的位置 等于 目标位置 解停止定时器
+        function animateTop(obj, target, callback) {
+            //bug ：当我们不断的点击按钮，相当于开启了太多的定时器，这个元素的速度会越来越快
+            // 解决方法：先清除以前的定时器，让元素只有一个定时器执行 如下：
+            clearInterval(obj.timer);
+            obj.timer = setInterval(function () {
+                // 步长公式因为有 现在的位置 的参数 所以要写到定时器里面
+                // 步长值改为整数，不要出现小数，否则就不准了 Math.ceil()
+                // 先声明一个变量
+                var step = (target - obj.offsetTop) / 10;
+                // 考虑盒子需要向左移动 也就是 步长为负值，此时就需要往小取整 Math.floor()
+                step = step > 0 ? Math.ceil(step) : Math.floor(step);
+                if (obj.offsetTop == target) {
+                    //停止动画 本质是停止定时器
+                    clearInterval(obj.timer);
+                    // 如果callback 为 true 则调用名为 callback 的这个函数
+                    if (callback) {
+                        callback();
+                    }
+                }
+                obj.style.top = obj.offsetTop + step + 'px';
+            }, 15)
+        }
---
+++ 四月份JavaScript/210419-网页轮播图的实现/品优购案例/shopping/js/index.js
@@ -0,0 +1,110 @@
+// 1.页面加载完成后启用js
+window.addEventListener('load', function () {
+// 2.获取元素
+    var focus = document.querySelector('.focus');
+    var arrow_l = document.querySelector('.arrow-l');
+    var arrow_r = document.querySelector('.arrow-r');
+    var focusWidth = focus.offsetWidth;
+
+// 3.鼠标经过轮播图，左右按钮显示，鼠标离开就隐藏
+    focus.addEventListener('mouseenter', function () {
+        arrow_l.style.display = 'block';
+        arrow_r.style.display = 'block';
+        clearInterval(timer);
+        timer = null; //清除定时器变量
+    })
+    focus.addEventListener('mouseleave', function () {
+        arrow_l.style.display = 'none';
+        arrow_r.style.display = 'none';
+        timer = setInterval(function () {
+            arrow_r.click();
+        }, 2000);
+    })
+// 4.动态生成小圆圈，有几张图片，就生成几个小圆圈
+    var ul = focus.querySelector('ul');
+    var ol = focus.querySelector('.circle');
+    // 遍历ul 的所有孩子
+    for (var i = 0; i < ul.children.length; i++) {
+        // 创建一个小li      
+        var li = document.createElement('li');
+        // 记录当前小li 的 索引号 通过添加自定义属性来得到
+        li.setAttribute('index', i);
+        // 把小li插入到 ol 里面
+        ol.appendChild(li);
+ // 5.小圆圈的排他思想，在生成小圆圈的同时直接绑定事件
+        li.addEventListener('click', function () {
+            // 先干掉所有人
+            for (var i = 0; i < ol.children.length; i++) {
+                ol.children[i].className = '';
+            }
+            // 再留下自己
+            this.className = 'current';
+ // 6.点击小圆圈，移动图片 注意 移动的是 ul 而不是 li
+            // ul 移动距离的算法：小圆圈的索引号 乘以 图片的宽度 注意是负值
+            // 当我们点击了某个小li 就拿到了当前小 li 的索引号
+            var index = this.getAttribute('index');
+            // 当我们点击了某个小li 就把这个小li 的索引号 给 num 就好了
+            num = index;
+             // 当我们点击了某个小li 就把这个小li 的索引号 给 circle 就好了
+            circle = index;
+            // 调用封装好的动画函数
+            animate(ul, -index * focusWidth);
+
+        })   
+    }
+    // 给 ol 里第一个 孩子 添加 current 的 类名
+    ol.children[0].className = 'current';
+    // 7.克隆第一张图片（li）放到最后面
+    var clone = ul.children[0].cloneNode(true);
+    ul.appendChild(clone);
+    // 8.点击右侧按钮，滚动轮播图一张
+    var num = 0;
+    var circle = 0;
+    arrow_r.addEventListener('click', function () {
+        if (num == ul.children.length - 1) {
+            ul.style.left = 0;
+            num = 0;
+        }
+        num++;
+        animate(ul, -num * focusWidth);
+    // 9.点击右侧按钮，小圆圈跟随一起变化，需要先声明一个变量控制小圆圈的播放
+        circle++;
+        // 如果circle == 4 说明走到最后一张克隆的图片了 就需要复原 
+        if (circle == ol.children.length) {
+            circle = 0;
+        }
+        // 先清除其余小圆圈的类名 排他思想
+        for (var i = 0; i < ol.children.length; i++) {
+            ol.children[i].className = '';
+        }
+        // 留下当前小圆圈的current 类名
+        ol.children[circle].className = 'current';
+    })
+    
+    // 10.左侧按钮的做法
+    arrow_l.addEventListener('click', function () {
+        if (num == 0) {
+            ul.style.left = -(ul.children.length - 1) * focusWidth + 'px';
+            num = ul.children.length - 1;
+        }
+        num--;
+        animate(ul, -num * focusWidth);
+    // 9.点击左侧按钮，小圆圈跟随一起变化，需要先声明一个变量控制小圆圈的播放
+        circle--;
+        // 如果circle < 0 说明走到第一张图片了 小圆圈就需要改为 第四个
+        if (circle < 0) {
+            circle = ol.children.length - 1;
+        }
+        // 先清除其余小圆圈的类名 排他思想
+        for (var i = 0; i < ol.children.length; i++) {
+            ol.children[i].className = '';
+        }
+        // 留下当前小圆圈的current 类名
+        ol.children[circle].className = 'current';
+    })
+    // // 10.自动播放轮播图功能  此功能写到鼠标经过，离开的事件里面
+    // var timer = setInterval(function () {
+    //     // 手动调用某个元素的点击事件
+    //     arrow_r.click();
+    // },2000)
+})
